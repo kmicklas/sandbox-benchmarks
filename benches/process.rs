c35142dcb580e5802ecbea25dfc8ef42bf92bf1a
Ken Micklas <git@kmicklas.com>
2023-04-11 20:58:27 +0100

    Abstract command running

--- benches/process.rs
+++ benches/process.rs
@@ -1,42 +1,26 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
-fn run_true() {
-    assert!(std::process::Command::new("true")
-        .status()
-        .unwrap()
-        .success())
-}
-
-fn run_brwap_true() {
-    assert!(std::process::Command::new("bwrap")
-        .args(["--ro-bind", "/", "/"])
-        .arg("true")
-        .status()
-        .unwrap()
-        .success())
-}
-
-fn run_brwap_unshare_true() {
-    assert!(std::process::Command::new("bwrap")
-        .arg("--unshare-all")
-        .args(["--ro-bind", "/", "/"])
-        .arg("true")
+fn run(cmd: &str, args: &[&str]) {
+    assert!(std::process::Command::new(cmd)
+        .args(args)
         .status()
         .unwrap()
         .success())
 }
 
 fn bench_true(c: &mut Criterion) {
-    c.bench_function("true", |b| b.iter(|| run_true()));
+    c.bench_function("true", |b| b.iter(|| run("true", &[])));
 }
 
 fn bench_bwrap_true(c: &mut Criterion) {
-    c.bench_function("bwrap true", |b| b.iter(|| run_brwap_true()));
+    c.bench_function("true", |b| {
+        b.iter(|| run("bwrap", &["--ro-bind", "/", "/", "true"]))
+    });
 }
 
 fn bench_bwrap_unshare_true(c: &mut Criterion) {
-    c.bench_function("bwrap unshare true", |b| {
-        b.iter(|| run_brwap_unshare_true())
+    c.bench_function("true", |b| {
+        b.iter(|| run("bwrap", &["--unshare-all", "--ro-bind", "/", "/", "true"]))
     });
 }
 
