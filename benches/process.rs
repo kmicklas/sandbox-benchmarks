8461c1eb2dd07aa01ba4e8429e6eb1ce1e0ac0d4
Ken Micklas <git@kmicklas.com>
2023-04-13 20:33:24 +0100

    Add unshare crate

--- benches/process.rs
+++ benches/process.rs
@@ -1,5 +1,9 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
+lazy_static::lazy_static! {
+    static ref TRUE: String = which::which("true").unwrap().to_string_lossy().into_owned();
+}
+
 fn run(cmd: &str, args: &[&str]) {
     assert!(std::process::Command::new(cmd)
         .args(args)
@@ -9,22 +13,42 @@ fn run(cmd: &str, args: &[&str]) {
 }
 
 fn bench_true(c: &mut Criterion) {
-    c.bench_function("true", |b| b.iter(|| run("true", &[])));
+    c.bench_function("true", |b| b.iter(|| run(&TRUE, &[])));
 }
 
 fn bench_sh_true(c: &mut Criterion) {
-    c.bench_function("sh true", |b| b.iter(|| run("sh", &["-c", "true"])));
+    c.bench_function("sh true", |b| b.iter(|| run("sh", &["-c", &TRUE])));
 }
 
 fn bench_bwrap_true(c: &mut Criterion) {
     c.bench_function("bwrap true", |b| {
-        b.iter(|| run("bwrap", &["--ro-bind", "/", "/", "true"]))
+        b.iter(|| run("bwrap", &["--ro-bind", "/", "/", &TRUE]))
     });
 }
 
 fn bench_bwrap_unshare_true(c: &mut Criterion) {
     c.bench_function("bwrap true (unshare)", |b| {
-        b.iter(|| run("bwrap", &["--unshare-all", "--ro-bind", "/", "/", "true"]))
+        b.iter(|| run("bwrap", &["--unshare-all", "--ro-bind", "/", "/", &TRUE]))
+    });
+}
+
+fn bench_direct_unshare_true(c: &mut Criterion) {
+    c.bench_function("direct unshare true", |b| {
+        b.iter(|| {
+            assert!(unshare::Command::new(&*TRUE)
+                .unshare(&[
+                    unshare::Namespace::Mount,
+                    unshare::Namespace::Uts,
+                    unshare::Namespace::Ipc,
+                    unshare::Namespace::User,
+                    unshare::Namespace::Pid,
+                    unshare::Namespace::Net,
+                    unshare::Namespace::Cgroup,
+                ])
+                .status()
+                .unwrap()
+                .success())
+        })
     });
 }
 
@@ -38,6 +62,7 @@ criterion_group!(
     bench_true,
     bench_sh_true,
     bench_bwrap_true,
-    bench_bwrap_unshare_true
+    bench_bwrap_unshare_true,
+    bench_direct_unshare_true,
 );
 criterion_main!(benches);
